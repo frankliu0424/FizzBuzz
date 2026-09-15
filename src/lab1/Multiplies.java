9aa789dd27dcd4c40b7bcf1b7561e66d942b3835
Frank Liu <fz.liu@mail.utoronto.ca>
2026-09-15 19:49:34 -0400

    refactored to extract helper method

--- src/lab1/Multiplies.java
+++ src/lab1/Multiplies.java
@@ -1,4 +1,19 @@
 package lab1;
 
 public class Multiplies {
+    public static void main(String[] args){
+        System.out.println(CountMultiplies());
+    }
+
+    private static int CountMultiplies() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++){
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy3 || divisibleBy5){
+                count++;
+            }
+        }
+        return count;
+    }
 }
