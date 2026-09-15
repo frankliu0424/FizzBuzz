5abb1caa7ec41b20190b88a46014f755224d52fd
Frank Liu <fz.liu@mail.utoronto.ca>
2026-09-15 19:48:39 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -10,33 +10,33 @@ class FizzBuzz {
     }
 
 
-    private static void doFizzBuzz() {
-        for (int i = 1; i <= 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-        }
-        int j = 1;
+    public static void doFizzBuzz() {
+//        for (int i = 1; i <= 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+//            }
+//        }
+       int j = 1;
         while (j <= 100) {
             boolean divisibleBy3 = j % 3 == 0;
             boolean divisibleBy5 = j % 5 == 0;
